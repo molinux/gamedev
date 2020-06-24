550eeb906db7ab9e04c687b2b41abe628937feeb
Molinux <cybercrowbr@gmail.com>
2020-06-23 22:46:33 -0300

    Double Jump, Sound Effects and Collision Detect

--- sketch.js
+++ sketch.js
@@ -1,33 +1,102 @@
 let imagemCenario;
 let imagemPersonagem;
+let imagemInimigo;
 let cenario;
 let somDoJogo;
+let somDoPulo;
 let personagem;
+let inimigo;
+
+const matrizInimigo = [
+  [0, 0],
+  [104, 0],
+  [208, 0],
+  [312, 0],
+  [0, 104],
+  [104, 104],
+  [208, 104],
+  [312, 104],
+  [0, 208],
+  [104, 208],
+  [208, 208],
+  [312, 208],
+  [0, 312],
+  [104, 312],
+  [208, 312],
+  [312, 312],
+  [0, 416],
+  [104, 416],
+  [208, 416],
+  [312, 416],
+  [0, 520],
+  [104, 520],
+  [208, 520],
+  [312, 520],
+  [0, 624],
+  [104, 624],
+  [208, 624],
+  [312, 624],
+]
+
+const matrizPersonagem = [
+  [0, 0],
+  [220, 0],
+  [440, 0],
+  [660, 0],
+  [0, 270],
+  [220, 270],
+  [440, 270],
+  [660, 270],
+  [0, 540],
+  [220, 540],
+  [440, 540],
+  [660, 540],
+  [0, 810],
+  [220, 810],
+  [440, 810],
+  [660, 810],
+]
 
 function preload() {
   imagemCenario = loadImage('imagens/cenario/floresta.png');
   imagemPersonagem = loadImage('imagens/personagem/correndo.png');
+  imagemInimigo = loadImage('imagens/inimigos/gotinha.png');
+  imagemGameOver = loadImage('imagens/assets/game-over.png');
   somDoJogo = loadSound('sons/trilha_jogo.mp3');
+  somDoPulo = loadSound('sons/somPulo.mp3');
 }
 
 function setup() {
-  // createCanvas(400, 400);
   createCanvas(windowWidth, windowHeight);
   cenario = new Cenario(imagemCenario, 3);
-  personagem = new Personagem(imagemPersonagem);
+  personagem = new Personagem(matrizPersonagem, imagemPersonagem, 0, 110, 135, 220, 270);
+  inimigo = new Inimigo(matrizInimigo, imagemInimigo, width - 52, 52, 52, 104,104);
   frameRate(40);
-  // somDoJogo.play();
-  // somDoJogo.loop();
+}
+
+function keyPressed() {
+  if (key === 'ArrowUp') {
+    personagem.pula();
+    somDoPulo.play();
+  }
 }
 
 function draw() {
-  // circle(mouseX, mouseY, 200);
-  // background(220);
-  // background(255,250,255);
-  // background(imagemCenario, -50, 0, width, height);
   cenario.exibe();
   cenario.move();
-  personagem.exibe()
+
+  personagem.exibe();
+  personagem.aplicaGravidade();
+
+  inimigo.exibe();
+  inimigo.move();
+
+  if(personagem.estaColidindo(inimigo)) {
+    console.log('Gotcha !!!');
+    noLoop();
+    // imageMode(CENTER);
+    image(imagemGameOver, 190, 170);
+  }
   
 }
 
